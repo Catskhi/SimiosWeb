4bb9e1698ebc41ca2b53bd46afb30d311cd60910
Enzo <enzopcontato@gmail.com>
2021-08-11 10:36:42 -0300

    Cookie

--- PHP/system/painel.php
+++ PHP/system/painel.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 include('verifica_login.php');
+$cookie_name = $_SESSION['usuario'];
+$cookie_value = $_SESSION['usuario'];
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
 ?>
 
 <h2>Olá <?php echo $_SESSION['usuario']; ?></h2>
